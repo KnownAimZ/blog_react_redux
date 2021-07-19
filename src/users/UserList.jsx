ee5f7856ea7562409e9dc7ad79d93a4f1f2919b6
Nikita <nbokii18@gmail.com>
2021-07-19 22:41:06 +0300

    userlist search

--- src/users/UserList.jsx
+++ src/users/UserList.jsx
@@ -1,18 +1,35 @@
+import './UserList.scss';
 import User from './User.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import React from 'react';
+
 
 import * as userActions from './users.action.js';
+import { useMemo } from 'react';
+
 const UserList = () => {
+    const [usersSearch, setUsersSearch] = React.useState('');
+    const users = useSelector(state=> state.users);
+    const handleChange = (event) => {
+        setUsersSearch(event.target.value);
+    };
     const dispatch = useDispatch();
     useEffect(()=>{
         dispatch(userActions.fetchUsers());
-    }, []);    
-    const users = useSelector(state=> state.users);
+    }, []);
+
     return (
         <>
         <h2>User List</h2>
-        {users.map(user => (<User key={user._id} {...user} />))}
+        <TextField          
+          label="Email"
+          value={usersSearch}
+          onChange={handleChange}
+          variant="outlined"
+        />        
+        {useMemo(()=> users.filter(user => user['email'].includes(usersSearch)).map(user => (<User key={user._id} {...user} />))) }
         </>
     );
 }; 
